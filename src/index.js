ed1877e5b25d89327ddf8e9d95ba6875f8c5c2d8
Yulia <Yulia159V@gmail.com>
2022-01-11 18:07:44 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function reverse (n) {
-  
+    if (n< 0) {
+        let num1 = Math.abs(n);
+        let str = String(num1);
+        let str1 = ''
+        for (let i = str.length - 1; i >= 0; i--){
+          str1 += str[i];
+        }
+        let result = Number(str1);
+        return result;
+      }
+      else {
+        let str = String(n);
+        let str1 = ''
+        for (let i = str.length - 1; i >= 0; i--){
+          str1 += str[i];
+        }
+        let result = Number(str1);
+        return result;
+      }
 }
